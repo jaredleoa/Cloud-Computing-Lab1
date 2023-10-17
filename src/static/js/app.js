31c151e1cc5b008fcf0b255819ae963af59151e5
jaredleoa <88289448+jaredleoa@users.noreply.github.com>
2023-10-17 12:52:36 +0100

    oops I can't remove it

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,6 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
+            <p className="text-center"></p>
             )}
             {items.map(item => (
                 <ItemDisplay
